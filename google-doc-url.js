c6fb8ffa61159cc95f68c8f0a16a7b3459b61577
likeaword <ben@satorilab.org>
2019-06-13 10:31:50 +0100

    Added new sheet address

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1dLfhSyebllpevQ_zDax-qtXhYemolJU6Zpo4z9uzYts/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1szDcgF19tnC2n_1PbCZFyqk0u_bY0vVNFcJ4Jjx7vY0/edit#gid=0';
